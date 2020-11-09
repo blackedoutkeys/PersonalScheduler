de9bcbcdb41674b48af64f97efb264ea06d462b3
Isaac Frishman <ifrishman89@gmail.com>
2020-11-09 15:46:43 -0600

    initial commit due to data loss

---
+++ script.js
@@ -0,0 +1,70 @@
+//global variables for hours
+
+var hour8 = $("#8");
+var hour9 = $("#9");
+var hour10 = $("#10");
+var hour11 = $("#11");
+var hour12 = $("#12");
+var hour1 = $("#13");
+var hour2 = $("#14");
+var hour3 = $("#15");
+var hour4 = $("#16");
+var hour5 = $("#17");
+
+var time = moment();
+
+
+
+// gets data for the header date
+function currentDate() {
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+    $("#dayAndTime").text(moment().format('h:mm:ss a'));  
+}
+
+
+//sets current date and saves user entry data
+function dayPlanner() {
+
+    currentDate();
+
+    $(".time-block").each(function () {
+        var id = $(this).attr("id");
+        var schedule = localStorage.getItem(id);
+
+        if (schedule !== null) {
+            $(this).children(".schedule").val(schedule);
+        }
+        
+    });
+}
+
+setInterval(dayPlanner, 1000);
+//activates save button within HTML
+dayPlanner ();
+var saveBtn = $(".saveBtn");
+//on click function
+saveBtn.on("click", function () {
+    var time = $(this).parent().attr("id");
+    var schedule = $(this).siblings(".schedule").val();
+
+    localStorage.setItem(time, schedule);
+});
+//sets the CSS to activate color changes based on the past/present/future hours to help organize
+function pastPresentFuture () {
+    hour = time.hours();
+    $(".time-block").each(function () {
+        var thisHour = parseInt($(this).attr("id"));
+
+        if (thisHour > hour) {
+            $(this).addClass("future");
+        }
+        else if (thisHour === hour) {
+            $(this).addClass("present");
+        }
+        else {
+            $(this).addClass("past");
+        }
+    });
+}
+//calls the function
+pastPresentFuture ();
